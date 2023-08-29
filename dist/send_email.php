5db46615e63901994ef1381e070046ebee167d1e
Mohammad Reza Gerami <mr.gerami@gmail.com>
2023-08-29 15:51:21 -0400

    Add files via upload

---
+++ dist/send_email.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "mrddatastu@gmail.com";
+    $subject = "New Contact Form Submission - github";
+    $headers = "From: $email";
+
+    mail($to, $subject, $message, $headers);
+
+    echo "Thank you for contacting us!";
+}
+?>
